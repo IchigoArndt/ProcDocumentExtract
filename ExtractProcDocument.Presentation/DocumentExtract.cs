3278cd178a156e9f41bfe2236b061e8a75e2e0bf
Luiz Felipe Arndt <arndtfelipe@gmail.com>
2019-07-24 01:53:02 -0300

    Criado e finalizado a ferramenta, basta testar e utilizar

---
+++ ExtractProcDocument.Presentation/DocumentExtract.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractProcDocument.Presentation
+{
+    public static class DocumentExtract
+    {
+        static string DocumentOriginal;
+        public static string documentOriginal(string tpDocument,string procDocument)
+        {
+            var proc = procDocument;
+            if (tpDocument.Equals("CTe"))
+            {
+                var indexStart = proc.IndexOf("<CTe");
+                var indexEnd = proc.IndexOf("<Signature ");
+                var text = proc.Substring(indexStart, indexEnd);
+                indexEnd = text.IndexOf("<Signature ");
+                text = text.Remove(indexEnd);
+                DocumentOriginal = "<enviCTe xmlns=\"http://www.portalfiscal.inf.br/cte\" versao=\"3.00\"><idLote>000000000000001</idLote>";
+                DocumentOriginal += "\n" + text;
+                DocumentOriginal += "\n" + "</CTe>" + "\n" + "</enviCTe>";
+            } else if (tpDocument.Equals("NFe"))
+            {
+                var indexStart = proc.IndexOf("<NFe");
+                var indexEnd = proc.IndexOf("<Signature ");
+                var text = proc.Substring(indexStart, indexEnd);
+                indexEnd = text.IndexOf("<Signature ");
+                text = text.Remove(indexEnd);
+                DocumentOriginal = "<enviNFe xmlns=\"http://www.portalfiscal.inf.br/nfe\" versao=\"4.00\"><idLote>000000000000001</idLote>";
+                DocumentOriginal += "\n" + text;
+                DocumentOriginal += "\n" + "</NFe>" + "\n" + "</enviNFe>";
+            }
+            else
+            {
+                var indexStart = proc.IndexOf("<MDFe");
+                var indexEnd = proc.IndexOf("<Signature ");
+                var text = proc.Substring(indexStart, indexEnd);
+                indexEnd = text.IndexOf("<Signature ");
+                text = text.Remove(indexEnd);
+                DocumentOriginal = "<enviMDFe xmlns=\"http://www.portalfiscal.inf.br/mdfe\" versao=\"3.00\"><idLote>000000000000001</idLote>";
+                DocumentOriginal += "\n" + text;
+                DocumentOriginal += "\n" + "</MDFe>" + "\n" + "</enviMDFe>";
+            }
+            return DocumentOriginal;
+        }
+    }
+}
---
+++ ExtractProcDocument.Presentation/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace ExtractProcDocument.Presentation
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnExtract.IsEnabled = false;
+            txtDocument.IsEnabled = true;
+            txtDocument.IsReadOnly = true;
+        }
+
+        private void BtnExtract_Click(object sender, RoutedEventArgs e)
+        {
+            var proc = new TextRange(txtProc.Document.ContentStart, txtProc.Document.ContentEnd);
+            if ((proc.Text != null)&&(proc.Text != string.Empty))
+            {
+                if (chkCTe.IsChecked == true)
+                {
+                    txtDocument.Text = DocumentExtract.documentOriginal("CTe", proc.Text.ToString());
+                }
+                else if (chkNFe.IsChecked == true)
+                {
+                    txtDocument.Text = DocumentExtract.documentOriginal("NFe", proc.Text.ToString());
+                }
+                else
+                {
+                    txtDocument.Text = DocumentExtract.documentOriginal("MDFe", proc.Text.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Proc Vazio !");
+            }
+        }
+
+        private void ChkNFe_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chkNFe.IsChecked == true)
+            {
+                chkCTe.IsEnabled = false;
+                chkMDFe.IsEnabled = false;
+                btnExtract.IsEnabled = true;
+            }
+            else
+            {
+                chkCTe.IsEnabled = true;
+                chkMDFe.IsEnabled = true;
+                btnExtract.IsEnabled = false;
+            }
+        }
+
+        private void ChkCTe_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chkCTe.IsChecked == true)
+            {
+                chkNFe.IsEnabled = false;
+                chkMDFe.IsEnabled = false;
+                btnExtract.IsEnabled = true;
+            }
+            else
+            {
+                chkNFe.IsEnabled = true;
+                chkMDFe.IsEnabled = true;
+                btnExtract.IsEnabled = false;
+            }
+        }
+
+        private void ChkMDFe_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chkMDFe.IsChecked == true)
+            {
+                chkCTe.IsEnabled = false;
+                chkNFe.IsEnabled = false;
+                btnExtract.IsEnabled = true;
+            }
+            else
+            {
+                chkNFe.IsEnabled = true;
+                chkCTe.IsEnabled = true;
+                btnExtract.IsEnabled = false;
+            }
+        }
+    }
+}
